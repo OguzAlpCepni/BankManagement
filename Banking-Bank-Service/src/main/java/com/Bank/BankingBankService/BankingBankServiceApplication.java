cd6650b28778aafe71f04f6d1b9925b85c262acf
oğuz alp çepni <oguzalpcepni@gmail.com>
2023-11-08 13:03:32 +0300

    Bank Service started

---
+++ Banking-Bank-Service/src/main/java/com/Bank/BankingBankService/BankingBankServiceApplication.java
@@ -0,0 +1,13 @@
+package com.Bank.BankingBankService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingBankServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingBankServiceApplication.class, args);
+	}
+
+}
